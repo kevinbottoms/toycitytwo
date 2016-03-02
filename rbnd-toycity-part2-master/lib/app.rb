b5110f5019c7ac2a7059266ff0ceb2a1a878b34e
Kevin Bottoms <webdev@kevinbottoms.com>
2016-03-02 11:40:24 +0100

    Refactored Code: setup_files to method date  to method product_ascii  to method brands_ascii to method create_brands_hash to method create_products_hash to method Changes to be committed: modified:   rbnd-toycity-part2-master/lib/app.rb new file:   rbnd-toycity-part2-master/lib/report.txt

--- rbnd-toycity-part2-master/lib/app.rb
+++ rbnd-toycity-part2-master/lib/app.rb
@@ -1,21 +1,28 @@
 require 'json'
-path = File.join(File.dirname(__FILE__), '../data/products.json')
-file = File.read(path)
-products_hash = JSON.parse(file)
 
-# Print today's date
-date = Time.now.strftime("%d/%m/%y %H:%M")
-puts "Date and time report generated: " + date
+def setup_files
+  path = File.join(File.dirname(__FILE__), '../data/products.json')
+  file = File.read(path)
+  $products_hash = JSON.parse(file)
+  $report_file = File.new("report.txt", "w+")
+end
 
-puts "                     _            _       "
-puts "                    | |          | |      "
-puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
-puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
-puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
-puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
-puts "| |                                       "
-puts "|_|                                       "
-puts
+def print_date
+  date = Time.now.strftime("%d/%m/%y %H:%M")
+  puts "Date and time report generated: " + date
+end
+
+def print_products_ascii
+  puts "                     _            _       "
+  puts "                    | |          | |      "
+  puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+  puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+  puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+  puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+  puts "| |                                       "
+  puts "|_|                                       "
+  puts
+end
 
 # For each product in the data set:
   # Print the name of the toy
@@ -25,29 +32,45 @@ puts
   # Calculate and print the average price the toy sold for
   # Calculate and print the average discount (% or $) based off the average sales price
 
-products = {}
-brands = {}
-
-products_hash["items"].each do |product|
-  if brands.keys.include? product["brand"]
-    brands[product["brand"]]["stock"] += product["stock"]
-    brands[product["brand"]]["amount_sold"] += product["purchases"].length
-    product["purchases"].each do |sale|
-      brands[product["brand"]]["total_sales"] += sale["price"]
-    end
-  else
-    brands[product["brand"]] = {}
-    brands[product["brand"]]["stock"] = product["stock"]
-    brands[product["brand"]]["amount_sold"] = product["purchases"].length
-    brands[product["brand"]]["total_sales"] = 0.0
-    product["purchases"].each do |sale|
-      brands[product["brand"]]["total_sales"] += sale["price"]
-    end
-  end
+def create_products_hash
+  toys = {}
+  $products_hash["items"].each do |product|
+    toys[product["title"]] = {}
+		toys[product["title"]]["retail_price"] = product["full-price"]
+		toys[product["title"]]["amount_sold"] = product["purchases"].length
+		toys[product["title"]]["total_sales"] = 0.0
+		product["purchases"].each do |sales|
+      toys[product["title"]]["total_sales"] += sales["price"]
+		end
+	end
+	return toys
 end
 
-puts brands
+def create_brands_hash
+	brands = {}
+  $products_hash["items"].each do |product|
+    if brands.keys.include? product["brand"]
+      brands[product["brand"]]["stock"] += product["stock"]
+      brands[product["brand"]]["amount_sold"] += product["purchases"].length
+      product["purchases"].each do |sale|
+        brands[product["brand"]]["total_sales"] += sale["price"]
+      end
+    else
+      brands[product["brand"]] = {}
+      brands[product["brand"]]["stock"] = product["stock"]
+      brands[product["brand"]]["amount_sold"] = product["purchases"].length
+      brands[product["brand"]]["total_sales"] = 0.0
+      product["purchases"].each do |sale|
+        brands[product["brand"]]["total_sales"] += sale["price"]
+      end
+    end
+  end
+	return brands
+end
 
+
+
+def print_brands_ascii
 	puts " _                         _     "
 	puts "| |                       | |    "
 	puts "| |__  _ __ __ _ _ __   __| |___ "
@@ -55,9 +78,19 @@ puts brands
 	puts "| |_) | | | (_| | | | | (_| \\__ \\"
 	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
 	puts
+end
+
+
 
 # For each brand in the data set:
   # Print the name of the brand
   # Count and print the number of the brand's toys we stock
   # Calculate and print the average price of the brand's toys
   # Calculate and print the total revenue of all the brand's toy sales combined
+
+setup_files()
+
+brands = create_brands_hash()
+toys = create_products_hash()
+puts brands
+puts toys
