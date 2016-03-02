fa24c5a4f719060fdba32cd15e0f464064cc3e0a
Kevin Bottoms <webdev@kevinbottoms.com>
2016-03-02 14:59:10 +0100

    Added average_price to toys hash Added average_discount to toys hash Added total_sales to brands hash Added average_price to brands hash Changes to be committed: modified:   rbnd-toycity-part2-master/lib/app.rb

--- rbnd-toycity-part2-master/lib/app.rb
+++ rbnd-toycity-part2-master/lib/app.rb
@@ -5,12 +5,12 @@ def setup_files
   file = File.read(path)
   $products_hash = JSON.parse(file)
   $report_file = File.new("report.txt", "w+")
-end
+end # end setup_files method
 
 def print_date
   date = Time.now.strftime("%d/%m/%y %H:%M")
   puts "Date and time report generated: " + date
-end
+end # end print_date method
 
 def print_products_ascii
   puts "                     _            _       "
@@ -22,7 +22,7 @@ def print_products_ascii
   puts "| |                                       "
   puts "|_|                                       "
   puts
-end
+end # end print_products_ascii
 
 # For each product in the data set:
   # Print the name of the toy
@@ -39,12 +39,21 @@ def create_products_hash
 		toys[product["title"]]["retail_price"] = product["full-price"]
 		toys[product["title"]]["amount_sold"] = product["purchases"].length
 		toys[product["title"]]["total_sales"] = 0.0
+    toys[product["title"]]["average_discount"] = 0.0
 		product["purchases"].each do |sales|
       toys[product["title"]]["total_sales"] += sales["price"]
-		end
-	end
+		end # end total_sales loop
+    toys[product["title"]]["average_price"] =
+    toys[product["title"]]["total_sales"] /
+    toys[product["title"]]["amount_sold"]
+
+    toys[product["title"]]["average_discount"] = 100 -
+    (toys[product["title"]]["average_price"].to_f /
+    toys[product["title"]]["retail_price"].to_f * 100)
+    #100 - (average_price.to_f / retail_price.to_f * 100)
+  end # end each loop to create_products_hash
 	return toys
-end
+end # end create_products_hash method
 
 def create_brands_hash
 	brands = {}
@@ -54,7 +63,7 @@ def create_brands_hash
       brands[product["brand"]]["amount_sold"] += product["purchases"].length
       product["purchases"].each do |sale|
         brands[product["brand"]]["total_sales"] += sale["price"]
-      end
+      end # end each loop to calculate total_sales
     else
       brands[product["brand"]] = {}
       brands[product["brand"]]["stock"] = product["stock"]
@@ -62,11 +71,14 @@ def create_brands_hash
       brands[product["brand"]]["total_sales"] = 0.0
       product["purchases"].each do |sale|
         brands[product["brand"]]["total_sales"] += sale["price"]
-      end
-    end
-  end
+      end # end each loop to calculate total_sales
+    end # end if to check if brand is already in brands hash
+    brands[product["brand"]]["average_price"] =
+    brands[product["brand"]]["total_sales"] /
+    brands[product["brand"]]["amount_sold"]
+  end # end each loop to create_brands_hash
 	return brands
-end
+end # end create_brands_hash method
 
 
 
@@ -78,7 +90,7 @@ def print_brands_ascii
 	puts "| |_) | | | (_| | | | | (_| \\__ \\"
 	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
 	puts
-end
+end # end print_brands_ascii method
 
 
 
